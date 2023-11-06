99c56f7305b9306eb37ad4df4dd6e913319a6ce4
sahilgarg9911 <105276593+sahilgarg9911@users.noreply.github.com>
2023-11-06 12:33:33 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
-import express from "express";
-import mongoose from "mongoose";
-import bodyParser from "body-parser"
-import dotenv from "dotenv";
-import cors from "cors";
-import route from "./routes/userRoute.js";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {Toaster} from "react-hot-toast";
+import './index.css';
+import App from './App';
 
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
-dotenv.config();
-
-
-const PORT = process.env.PORT || 7000;
-const URL = process.env.MONGOURL;
-
-mongoose.connect(URL).then(()=>{
-
-    console.log("DB connected successfully");
-
-    app.listen(PORT, ()=>{
-        console.log(`Server is running on port: ${PORT}`);
-    })
-
-}).catch(error => console.log(error));
-
-
-app.use("/api", route);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    <Toaster/>
+  </React.StrictMode>
+);
